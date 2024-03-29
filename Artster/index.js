3ed27a3a1d274d4235e0199b57c6b452118c7ec6
arun <crcravi66@gmail.com>
2024-03-29 16:14:24 +0530

    practice a expresse

---
+++ Artster/index.js
@@ -0,0 +1,33 @@
+const figlet = require("figlet");
+const colors = require("@colors/colors")
+
+figlet("Hello World!!", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data.rainbow);
+  });
+
+            // Bbbboo
+
+figlet.text(
+  "Boo!",
+  {
+    font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 80,
+    whitespaceBreak: true,
+  },
+  function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data.trap);
+  }
+);
+
